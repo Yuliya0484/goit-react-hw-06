b690a31c701f83580356425d128ff02c4a89c8eb
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-01-22 10:17:16 +0100

    main-settings

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const initialState = {
+  contacts: {
+    items: [],
+  },
+  filters: {
+    name: "",
+  },
+};
+
+const rootReducer = (state, action) => {
+  return state;
+};
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
