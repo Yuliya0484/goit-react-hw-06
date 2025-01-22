a7a5669f3fc3ec402456baa188c440e24cdee209
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-01-22 22:33:37 +0100

    redux-file-done

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import { useEffect, useState } from "react";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const initialContacts = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "./contactsSlice";
+import { filtersReducer } from "./filtersSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const initialState = {
-  contacts: {
-    items: [],
-  },
-  filters: {
-    name: "",
-  },
+const contactsPersistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["items"],
 };
 
-const rootReducer = (state, action) => {
-  return state;
-};
+const persistedContactsReducer = persistReducer(
+  contactsPersistConfig,
+  contactsReducer
+);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
 });
+export const persistor = persistStore(store);
