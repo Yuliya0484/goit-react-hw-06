222fcfeaacade587acf8168291d1341f83d2ed60
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2025-01-24 11:29:34 +0100

    fixed-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,14 +7,20 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "contacts",
   storage,
+  whitelist: ["items"],
 };
 
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
 export const persistor = persistStore(store);
